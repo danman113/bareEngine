28d4d65ea1a4135afd500015f041bc1f14adb9d6
danman113 <danman113@msn.com>
2015-01-24 05:30:07 -0800

    Made engine more Object-Oriented
    Now you create an engine with the "engine" class.

--- content.js
+++ content.js
@@ -1,14 +1,21 @@
 var imageUrls=["http://danman113.vacau.com/4041.jpg","http://danman113.vacau.com/4042.jpg","http://danman113.vacau.com/4040.jpg"]
-function disp(){
-	c.clearRect(0,0,width,height);
+var bareEngine=new engine(400,300);
+
+var c;
+function init(){
+	bareEngine.init();
+	c=bareEngine.context;
+}
+
+function draw(){
+	c.clearRect(0,0,bareEngine.width,bareEngine.height);
 	c.fillRect(10,10,10,10);
 	c.fillText("Map data: ", 30,30);
 	for(var i=0;i<mapdata.length;i++){
 		c.fillText(mapdata[i],80+i*5,30);
 	}
-	for(var i=0;i<images.length;i++){
-		c.drawImage(images[i],0+i*42,75,40,40);
+	for(var i=0;i<bareEngine.images.length;i++){
+		c.drawImage(bareEngine.images[i],0+i*42,75,40,40);
 	}
-	
-	disploop=requestAnimationFrame(disp);
+	requestAnimationFrame(draw);
 }
--- engine.js
+++ engine.js
@@ -1,32 +1,44 @@
-var c;
-var canvas;
-var width=400;
-var height=300;
-var loaded=false;
-var disploop;
-var images=[];
-var imagesLoaded=0;
-var totalImages;
-function init(){
-	document.body.innerHTML+="<canvas id='canvas' width='"+width+"' height='"+height+"'></canvas>";
-	canvas=document.getElementById("canvas");
-	c=canvas.getContext("2d");
-	totalImages=imageUrls.length;
-	disploop=requestAnimationFrame(disp);
-	loadImages(imageUrls);
-}
 
-function loadImages(imageUrls){
-	for(var i=0;i<imageUrls.length;i++){
-		images.push(new Image);
-		images[i].src=imageUrls[i];
-		var name=imageUrls[i];
-		images[i].onload=function(){onImageLoad(name)};
-		
+function engine(width,height){
+	this.version=0.1;
+	this.width=width;
+	this.height=height;
+	this.images=[];
+	this.imagesLoaded=0;
+	this.totalImages=this.images.length;
+	this.canvas;
+	this.context;
+	this.displayLoop;
+	this.displayFunction;
+	this.currentScreen=-1;
+	this.scenes=[];
+	this.loaded=false;
+	this.errorMessage="bareEngine error: ";
+	this.init=function(){
+		document.body.innerHTML+="<canvas id='canvas' width='"+width+"' height='"+height+"'></canvas>";
+		this.canvas=document.getElementById("canvas");
+		this.context=canvas.getContext("2d");
+		this.totalImages=imageUrls.length;
+		if(typeof draw == "function")
+			this.displayLoop=window.requestAnimationFrame(draw);
+		else
+			throw this.errorMessage+"No draw function found. Add a draw function if you want a game loop.";
+		this.loadImages(imageUrls);
+		this.loaded=true;
+	}
+
+	this.onImageLoad=function(name){
+		console.log(name + " was loaded!");
+		this.imagesLoaded++;
+	}
+
+	this.loadImages=function(imageUrls){
+		for(var i=0;i<imageUrls.length;i++){
+			this.images.push(new Image);
+			this.images[i].src=imageUrls[i];
+			var name=imageUrls[i];
+			this.images[i].onload=this.onImageLoad(name);
+		}
 	}
 }
 
-function onImageLoad(name){
-	console.log(name + " was loaded!");
-	imagesLoaded++;
-}
