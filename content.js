c233d7238464e8f00ec72c00a8b2e652c68c78d9
danman113 <danman113@msn.com>
2015-01-25 00:24:08 -0800

    Audio/Input
    Added support for managing and playing audio, as well as support for
    mouse input.

--- content.js
+++ content.js
@@ -1,21 +1,61 @@
-var imageUrls=["http://danman113.vacau.com/4041.jpg","http://danman113.vacau.com/4042.jpg","http://danman113.vacau.com/4040.jpg"]
-var bareEngine=new engine(400,300);
+/*
+Current demo showcases:
+Loading Music, images, and files seamlessly
+Good input handling
+Collision
+Working music system
+*/
 
+var engine=new bareEngine(400,300);
+engine.imageUrls=["http://danman113.vacau.com/4041.jpg","http://danman113.vacau.com/4042.jpg","http://danman113.vacau.com/4040.jpg"]
+engine.audioUrls=["http://neveronti.me/Rogue_Psycho/pickup.mp3","http://neveronti.me/Assault_Fury/Necro.mp3"];
+engine.defaultVolume=.25;
 var c;
+var fcount=0;
 function init(){
-	bareEngine.init();
-	c=bareEngine.context;
+	engine.init(document.body);
+	c=engine.context;
 }
 
 function draw(){
-	c.clearRect(0,0,bareEngine.width,bareEngine.height);
-	c.fillRect(10,10,10,10);
-	c.fillText("Map data: ", 30,30);
-	for(var i=0;i<mapdata.length;i++){
-		c.fillText(mapdata[i],80+i*5,30);
-	}
-	for(var i=0;i<bareEngine.images.length;i++){
-		c.drawImage(bareEngine.images[i],0+i*42,75,40,40);
+	fcount++;
+	engine.mouseManager();
+	switch(engine.currentScene){
+		case -1:
+			if(globalBare.imagesLoaded+globalBare.audioLoaded>=engine.totalImages+engine.totalAudio)
+				engine.currentScene=0;
+			c.fillStyle="grey";
+			c.fillRect(10,50,100,40);
+			c.fillStyle="red";
+			c.fillRect(10,50,100*((globalBare.imagesLoaded+globalBare.audioLoaded)/(engine.totalImages+engine.totalAudio)),40);
+			c.fillStyle="black";
+			c.fillText((globalBare.imagesLoaded+globalBare.audioLoaded)+"/"+(engine.totalImages+engine.totalAudio)+" Assets Loaded",15,60);
+		break;
+		case 0:
+			c.clearRect(0,0,engine.width,engine.height);
+			
+			engine.audio[1].element.paused?c.fillStyle="black":c.fillStyle="red";
+			c.fillRect(10,10,10,10);
+			c.fillStyle="black";
+			c.fillText("< Click here for music",22,18);
+			if(engine.rectCollision(globalBare.mouse.x,globalBare.mouse.y,1,1,10,10,10,10) && globalBare.mouseUp){
+				if(engine.audio[1].element.paused)
+				engine.audio[1].play();
+				else
+				engine.audio[1].stop();
+			}
+			c.fillText("Map data: ", 30,30);
+			for(var i=0;i<mapdata.length;i++){
+				c.fillText(mapdata[i],80+i*5,30);
+			}
+			for(var i=0;i<engine.images.length;i++){
+				c.drawImage(engine.images[i],0+i*42,75,40,40);
+			}
+			if(!globalBare.mouse.up){
+				engine.audio[0].play();
+			}
+			
+		break;
 	}
 	requestAnimationFrame(draw);
 }
--- engine.js
+++ engine.js
@@ -1,44 +1,134 @@
-
-function engine(width,height){
-	this.version=0.1;
+var globalBare={
+	imagesLoaded:0,audioLoaded:0,mouse:{x:0,y:0,up:true,hover:-1,mouseUp:false},loaded:false
+}
+function bareEngine(width,height){
+	//Engine information
+	this.version=0.2;
+	//Options
+	this.defaultVolume=0.5;
+	//Canvas
 	this.width=width;
 	this.height=height;
-	this.images=[];
-	this.imagesLoaded=0;
-	this.totalImages=this.images.length;
 	this.canvas;
 	this.context;
 	this.displayLoop;
-	this.displayFunction;
-	this.currentScreen=-1;
+	this.target;
+	//Media
+	this.images=[];
+	this.audio=[];
+	this.imageUrls=[];
+	this.audioUrls=[];
+	this.totalImages=this.images.length;
+	this.totalAudio=this.audio.length;
+	//Scene
+	this.currentScene=-1;
 	this.scenes=[];
 	this.loaded=false;
 	this.errorMessage="bareEngine error: ";
-	this.init=function(){
-		document.body.innerHTML+="<canvas id='canvas' width='"+width+"' height='"+height+"'></canvas>";
+	this.lastMouseUp=true;
+	this.init=function(target){
+		//Creating the canvas
+		this.target=target;
+		this.target.innerHTML+="<canvas id='canvas' width='"+width+"' height='"+height+"'></canvas>";
 		this.canvas=document.getElementById("canvas");
 		this.context=canvas.getContext("2d");
-		this.totalImages=imageUrls.length;
+		globalBare.canvas=this.canvas;
+		//Input handling
+		this.canvas.addEventListener('mouseup', this.mouseUp, false);
+		this.canvas.addEventListener('mousedown', this.mouseDown, false);
+		this.canvas.addEventListener('mousemove', this.mouseMove, false);
+		//Initializing the draw loop
 		if(typeof draw == "function")
 			this.displayLoop=window.requestAnimationFrame(draw);
 		else
-			throw this.errorMessage+"No draw function found. Add a draw function if you want a game loop.";
-		this.loadImages(imageUrls);
-		this.loaded=true;
+			this.error("No draw function found. Add a draw function if you want a game loop.");
+		//Loading assets
+		this.totalImages=this.imageUrls.length;
+		this.totalAudio=this.audioUrls.length;
+		this.loadImages();
+		this.loadAudio();
 	}
-
-	this.onImageLoad=function(name){
-		console.log(name + " was loaded!");
-		this.imagesLoaded++;
+	//Input
+	this.mouseUp=function(e){
+		globalBare.mouse.up=true;
+		if(typeof globalBare.onMouseUp=="function")
+			globalBare.onMouseUp();
 	}
-
-	this.loadImages=function(imageUrls){
-		for(var i=0;i<imageUrls.length;i++){
-			this.images.push(new Image);
-			this.images[i].src=imageUrls[i];
-			var name=imageUrls[i];
-			this.images[i].onload=this.onImageLoad(name);
+	this.mouseDown=function(e){
+		globalBare.mouse.up=false;
+		if(typeof globalBare.onMouseDown=="function")
+			globalBare.onMouseDown();
+	}
+	this.mouseMove=function(e){
+		if(e.offsetX) {
+			globalBare.mouse.x = e.offsetX;
+			globalBare.mouse.y = e.offsetY;
+		} else if(e.layerX) {
+		var box=globalBare.canvas.getBoundingClientRect();
+			globalBare.mouse.x = e.layerX-box.left;
+			globalBare.mouse.y = e.layerY-box.top;
 		}
+	}
+	//Media Loading
+	this.onImageLoad=function(){
+		globalBare.imagesLoaded++;
+		console.log("Image "+globalBare.imagesLoaded+" Loaded");
+	}
+
+	this.loadImages=function(){
+		for(var i=0;i<this.imageUrls.length;i++){
+			this.images.push(new Image);
+			this.images[i].onload=this.onImageLoad;
+			this.images[i].src=this.imageUrls[i];
+			var name=this.imageUrls[i];
+			this.context.drawImage(this.images[i],-1,-1,1,1);
+		}
+	}
+
+	this.loadAudio=function(){
+		for(var i=0;i<this.totalAudio;i++){
+			this.audio.push(new audioFile(this.audioUrls[i]));
+			this.audio[i].element=document.createElement('audio');
+			this.audio[i].element.src=this.audio[i].src;
+			this.audio[i].element.volume=this.defaultVolume;
+			this.audio[i].element.oncanplaythrough=this.onAudioLoad;
+		}
+	}
+
+	this.onAudioLoad=function(){
+		if((globalBare.imagesLoaded+globalBare.audioLoaded)<engine.totalImages+engine.totalAudio){
+			globalBare.audioLoaded++;
+			console.log("Can play");
+		}
+	}
+	//System functions
+	this.error=function(message){
+		throw this.errorMessage+message;
+	}
+
+	this.rectCollision=function(mousex,mousey,mousew,mouseh,x,y,w,h){
+		return mousex+mousew > x && mousex < (x + w) && mousey+mouseh > y && mousey < (y + h);
+	}
+	this.mouseManager=function(){
+		if(!this.lastMouseUp && globalBare.mouse.up){
+			globalBare.mouseUp=true;
+		} else {
+			globalBare.mouseUp=false;
+		}
+		this.lastMouseUp=globalBare.mouse.up;
 	}
 }
 
+function audioFile(src,defaultVolume){
+	this.src=src;
+	this.element;
+	this.play=function(volume){
+		if(volume!==undefined && this.element.volume!=volume)
+			this.element.volume=volume;
+		this.element.play();
+	}
+	this.stop=function(){
+		this.element.pause();
+		this.element.currentTime = 0;
+	}
+}
